2316b98f9aa511050d8f480d8dec56dcf2ea4ed5
jaren.xiu <jarenmarcelino76@gmail.com>
2026-02-21 20:03:23 +0800

    Aadded srtf

--- schedule.cpp
+++ schedule.cpp
@@ -3,43 +3,40 @@
 
 using namespace std;
 
+// Structure to represent a Process
+// Holds all input parameters, tracking variables
 struct Process {
-    int id;
-    int arrivalTime;
-    int burstTime;
-    
-    // Stats
-    int startTime;
-    int finishTime;
-    int waitingTime;
-    int turnaroundTime;
-    int responseTime;
-    
-    // Logic flag for SJF
-    bool isCompleted;
+    int id;                 
+    int arrivalTime;        
+    int burstTime;          
+    int remainingTime;      
+    int startTime;          
+    int finishTime;         
+    int waitingTime;        
+    int turnaroundTime;     
+    int responseTime;       
+    bool isCompleted;       
 };
 
-// Bubble Sort
-// Sorts by Arrival Time (Ascending), then by ID (Ascending)
+// Performs a bubble sort on the processes vector. 
+// It sorts primarily by arrival time (Ascending), and uses the process id
+// (Ascending) as a tie-breaker if multiple processes arrive at the exact same time.
 void sortProcesses(vector<Process> &procs) {
     int n = procs.size();
+    
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            
             bool shouldSwap = false;
             
-            // Check 1: Arrival Time
             if (procs[j].arrivalTime > procs[j+1].arrivalTime) {
                 shouldSwap = true;
             } 
-            // Check 2: Tie-Breaker (ID)
             else if (procs[j].arrivalTime == procs[j+1].arrivalTime) {
                 if (procs[j].id > procs[j+1].id) {
                     shouldSwap = true;
                 }
             }
 
-            // Perform Swap
             if (shouldSwap) {
                 Process temp = procs[j];
                 procs[j] = procs[j+1];
@@ -50,43 +47,32 @@ void sortProcesses(vector<Process> &procs) {
 }
 
 void fcfs(vector<Process> procs, int testCaseNum) {
-    // USE MANUAL SORT
     sortProcesses(procs);
-
     cout << testCaseNum << " FCFS" << endl;
-    
     int currentTime = 0;
     
-    // Gantt Chart
     for (int i = 0; i < procs.size(); i++) {
-        // IDLE CHECK
         if (currentTime < procs[i].arrivalTime) {
             currentTime = procs[i].arrivalTime;
         }
         
-        // Start & Finish logic
         procs[i].startTime = currentTime;
-        
         cout << currentTime << " " << procs[i].id << " " << procs[i].burstTime << "X" << endl;
         
         currentTime = currentTime + procs[i].burstTime;
         procs[i].finishTime = currentTime;
         
-        // Calculate Metrics
         procs[i].turnaroundTime = procs[i].finishTime - procs[i].arrivalTime;
         procs[i].waitingTime = procs[i].startTime - procs[i].arrivalTime;
         procs[i].responseTime = procs[i].startTime - procs[i].arrivalTime;
     }
     
-    // --- METRICS OUTPUT ---
     cout << "Total time elapsed: " << currentTime << "ns" << endl;
     
-    double totalWait = 0;
-    double totalTurnaround = 0;
-    double totalResponse = 0;
+    double totalWait = 0, totalTurnaround = 0, totalResponse = 0;
     int totalBurst = 0;
     
-    for(int i = 0; i < procs.size(); i++) {
+    for (int i = 0; i < procs.size(); i++) {
         totalWait = totalWait + procs[i].waitingTime;
         totalTurnaround = totalTurnaround + procs[i].turnaroundTime;
         totalResponse = totalResponse + procs[i].responseTime;
@@ -94,36 +80,25 @@ void fcfs(vector<Process> procs, int testCaseNum) {
     }
     
     cout << "Total CPU burst time: " << totalBurst << "ns" << endl;
-    
     double utilization = ((double)totalBurst / currentTime) * 100.0;
     cout << "CPU Utilization: " << utilization << "%" << endl;
-    
     cout << "Throughput: " << (double)procs.size() / currentTime << " processes/ns" << endl;
 
     cout << "Waiting times:" << endl;
-    for(int i=0; i<procs.size(); i++) {
-        cout << " Process " << procs[i].id << ": " << procs[i].waitingTime << "ns" << endl;
-    }
+    for (int i = 0; i < procs.size(); i++) cout << " Process " << procs[i].id << ": " << procs[i].waitingTime << "ns" << endl;
     cout << "Average waiting time: " << totalWait / procs.size() << "ns" << endl;
 
     cout << "Turnaround times:" << endl;
-    for(int i=0; i<procs.size(); i++) {
-        cout << " Process " << procs[i].id << ": " << procs[i].turnaroundTime << "ns" << endl;
-    }
+    for (int i = 0; i < procs.size(); i++) cout << " Process " << procs[i].id << ": " << procs[i].turnaroundTime << "ns" << endl;
     cout << "Average turnaround time: " << totalTurnaround / procs.size() << "ns" << endl;
 
     cout << "Response times:" << endl;
-    for(int i=0; i<procs.size(); i++) {
-        cout << " Process " << procs[i].id << ": " << procs[i].responseTime << "ns" << endl;
-    }
+    for (int i = 0; i < procs.size(); i++) cout << " Process " << procs[i].id << ": " << procs[i].responseTime << "ns" << endl;
     cout << "Average response time: " << totalResponse / procs.size() << "ns" << endl;
 }
 
-// --- SJF ALGORITHM ---
 void sjf(vector<Process> procs, int testCaseNum) {
-    // USE MANUAL SORT
     sortProcesses(procs);
-
     cout << testCaseNum << " SJF" << endl;
 
     int currentTime = 0;
@@ -133,24 +108,19 @@ void sjf(vector<Process> procs, int testCaseNum) {
     while (completedCount < n) {
         int shortestIdx = -1;
         
-        // Find best process in Ready Queue
         for (int i = 0; i < n; i++) {
             if (procs[i].arrivalTime <= currentTime && !procs[i].isCompleted) {
-                
                 if (shortestIdx == -1) {
                     shortestIdx = i;
                 } 
                 else {
-                    // SJF Logic: Compare Burst
                     if (procs[i].burstTime < procs[shortestIdx].burstTime) {
                         shortestIdx = i;
                     }
-                    // Tie-breaker 1: Arrival Time
                     else if (procs[i].burstTime == procs[shortestIdx].burstTime) {
                         if (procs[i].arrivalTime < procs[shortestIdx].arrivalTime) {
                             shortestIdx = i;
                         } 
-                        // Tie-breaker 2: ID
                         else if (procs[i].arrivalTime == procs[shortestIdx].arrivalTime) {
                             if (procs[i].id < procs[shortestIdx].id) {
                                 shortestIdx = i;
@@ -162,9 +132,7 @@ void sjf(vector<Process> procs, int testCaseNum) {
         }
 
         if (shortestIdx != -1) {
-            // EXECUTE
             procs[shortestIdx].startTime = currentTime;
-            
             cout << currentTime << " " << procs[shortestIdx].id << " " << procs[shortestIdx].burstTime << "X" << endl;
 
             currentTime = currentTime + procs[shortestIdx].burstTime;
@@ -172,29 +140,22 @@ void sjf(vector<Process> procs, int testCaseNum) {
             procs[shortestIdx].isCompleted = true;
             completedCount++;
             
-            // Calculate Stats
             procs[shortestIdx].turnaroundTime = procs[shortestIdx].finishTime - procs[shortestIdx].arrivalTime;
             procs[shortestIdx].waitingTime = procs[shortestIdx].startTime - procs[shortestIdx].arrivalTime;
             procs[shortestIdx].responseTime = procs[shortestIdx].startTime - procs[shortestIdx].arrivalTime;
-
         } else {
-            // IDLE
             currentTime++;
         }
     }
     
-    // --- METRICS OUTPUT ---
     cout << "Total time elapsed: " << currentTime << "ns" << endl;
     
-    double totalWait = 0;
-    double totalTurnaround = 0;
-    double totalResponse = 0;
+    double totalWait = 0, totalTurnaround = 0, totalResponse = 0;
     int totalBurst = 0;
     
-    // Manual Bubble Sort by ID for final output
-    for(int i=0; i<n-1; i++) {
-        for(int j=0; j<n-i-1; j++) {
-            if(procs[j].id > procs[j+1].id) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (procs[j].id > procs[j+1].id) {
                 Process temp = procs[j];
                 procs[j] = procs[j+1];
                 procs[j+1] = temp;
@@ -202,7 +163,7 @@ void sjf(vector<Process> procs, int testCaseNum) {
         }
     }
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         totalWait = totalWait + procs[i].waitingTime;
         totalTurnaround = totalTurnaround + procs[i].turnaroundTime;
         totalResponse = totalResponse + procs[i].responseTime;
@@ -215,26 +176,145 @@ void sjf(vector<Process> procs, int testCaseNum) {
     cout << "Throughput: " << (double)n / currentTime << " processes/ns" << endl;
 
     cout << "Waiting times:" << endl;
-    for(int i=0; i<n; i++) {
-        cout << " Process " << procs[i].id << ": " << procs[i].waitingTime << "ns" << endl;
-    }
+    for (int i = 0; i < n; i++) cout << " Process " << procs[i].id << ": " << procs[i].waitingTime << "ns" << endl;
     cout << "Average waiting time: " << totalWait / n << "ns" << endl;
 
     cout << "Turnaround times:" << endl;
-    for(int i=0; i<n; i++) {
-        cout << " Process " << procs[i].id << ": " << procs[i].turnaroundTime << "ns" << endl;
-    }
+    for (int i = 0; i < n; i++) cout << " Process " << procs[i].id << ": " << procs[i].turnaroundTime << "ns" << endl;
     cout << "Average turnaround time: " << totalTurnaround / n << "ns" << endl;
 
     cout << "Response times:" << endl;
-    for(int i=0; i<n; i++) {
-        cout << " Process " << procs[i].id << ": " << procs[i].responseTime << "ns" << endl;
+    for (int i = 0; i < n; i++) cout << " Process " << procs[i].id << ": " << procs[i].responseTime << "ns" << endl;
+    cout << "Average response time: " << totalResponse / n << "ns" << endl;
+}
+
+void srtf(vector<Process> procs, int testCaseNum) {
+    sortProcesses(procs);
+    cout << testCaseNum << " SRTF" << endl;
+
+    int currentTime = 0;
+    int completedCount = 0;
+    int n = procs.size();
+
+    int lastProcessId = -1;
+    int startBlockTime = 0;
+    int currentBurst = 0;
+
+    while (completedCount < n) {
+        int shortestIdx = -1;
+        
+        for (int i = 0; i < n; i++) {
+            if (procs[i].arrivalTime <= currentTime && !procs[i].isCompleted) {
+                if (shortestIdx == -1) {
+                    shortestIdx = i;
+                } 
+                else {
+                    if (procs[i].remainingTime < procs[shortestIdx].remainingTime) {
+                        shortestIdx = i;
+                    }
+                    else if (procs[i].remainingTime == procs[shortestIdx].remainingTime) {
+                        if (procs[i].arrivalTime < procs[shortestIdx].arrivalTime) {
+                            shortestIdx = i;
+                        } 
+                        else if (procs[i].arrivalTime == procs[shortestIdx].arrivalTime) {
+                            if (procs[i].id < procs[shortestIdx].id) {
+                                shortestIdx = i;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (shortestIdx != -1) {
+            if (lastProcessId != -1 && procs[shortestIdx].id != lastProcessId) {
+                cout << startBlockTime << " " << lastProcessId << " " << currentBurst << endl;
+                startBlockTime = currentTime;
+                currentBurst = 0;
+            }
+
+            lastProcessId = procs[shortestIdx].id;
+
+            if (procs[shortestIdx].startTime == -1) {
+                procs[shortestIdx].startTime = currentTime;
+            }
+
+            procs[shortestIdx].remainingTime--;
+            currentBurst++;
+            currentTime++;
+
+            if (procs[shortestIdx].remainingTime == 0) {
+                procs[shortestIdx].finishTime = currentTime;
+                procs[shortestIdx].isCompleted = true;
+                completedCount++;
+                
+                cout << startBlockTime << " " << procs[shortestIdx].id << " " << currentBurst << "X" << endl;
+                
+                procs[shortestIdx].turnaroundTime = procs[shortestIdx].finishTime - procs[shortestIdx].arrivalTime;
+                procs[shortestIdx].waitingTime = procs[shortestIdx].turnaroundTime - procs[shortestIdx].burstTime;
+                procs[shortestIdx].responseTime = procs[shortestIdx].startTime - procs[shortestIdx].arrivalTime;
+
+                lastProcessId = -1; 
+                startBlockTime = currentTime;
+                currentBurst = 0;
+            }
+        } else {
+            if (lastProcessId != -1) {
+                 cout << startBlockTime << " " << lastProcessId << " " << currentBurst << endl;
+                 lastProcessId = -1;
+                 currentBurst = 0;
+            }
+            
+            currentTime++;
+            if (currentBurst == 0) {
+                startBlockTime = currentTime;
+            }
+        }
     }
+    
+    cout << "Total time elapsed: " << currentTime << "ns" << endl;
+    
+    double totalWait = 0, totalTurnaround = 0, totalResponse = 0;
+    int totalBurst = 0;
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (procs[j].id > procs[j+1].id) {
+                Process temp = procs[j];
+                procs[j] = procs[j+1];
+                procs[j+1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        totalWait = totalWait + procs[i].waitingTime;
+        totalTurnaround = totalTurnaround + procs[i].turnaroundTime;
+        totalResponse = totalResponse + procs[i].responseTime;
+        totalBurst = totalBurst + procs[i].burstTime;
+    }
+    
+    cout << "Total CPU burst time: " << totalBurst << "ns" << endl;
+    double utilization = ((double)totalBurst / currentTime) * 100.0;
+    cout << "CPU Utilization: " << utilization << "%" << endl;
+    cout << "Throughput: " << (double)n / currentTime << " processes/ns" << endl;
+
+    cout << "Waiting times:" << endl;
+    for (int i = 0; i < n; i++) cout << " Process " << procs[i].id << ": " << procs[i].waitingTime << "ns" << endl;
+    cout << "Average waiting time: " << totalWait / n << "ns" << endl;
+
+    cout << "Turnaround times:" << endl;
+    for (int i = 0; i < n; i++) cout << " Process " << procs[i].id << ": " << procs[i].turnaroundTime << "ns" << endl;
+    cout << "Average turnaround time: " << totalTurnaround / n << "ns" << endl;
+
+    cout << "Response times:" << endl;
+    for (int i = 0; i < n; i++) cout << " Process " << procs[i].id << ": " << procs[i].responseTime << "ns" << endl;
     cout << "Average response time: " << totalResponse / n << "ns" << endl;
 }
 
 int main() {
     int testCases;
+    
     if (!(cin >> testCases)) return 0;
 
     for (int t = 1; t <= testCases; t++) {
@@ -251,7 +331,11 @@ int main() {
             p.id = i + 1; 
             p.arrivalTime = arrival;
             p.burstTime = burst;
+            
+            p.remainingTime = burst; 
+            p.startTime = -1;        
             p.isCompleted = false;
+            
             processes.push_back(p);
         }
 
@@ -260,6 +344,9 @@ int main() {
         } 
         else if (algorithm == "SJF") {
             sjf(processes, t);
+        }
+        else if (algorithm == "SRTF") {
+            srtf(processes, t);
         }
     }
     return 0;
