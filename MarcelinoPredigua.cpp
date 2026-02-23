87c3a3769520660d28aa3dbfd3104af198e36a9f
jaren.xiu <jarenmarcelino76@gmail.com>
2026-02-23 22:05:24 +0800

    Added comments

--- MarcelinoPredigua.cpp
+++ MarcelinoPredigua.cpp
@@ -23,9 +23,10 @@ struct Process {
     int niceLevel;
 };
 
-// Performs a bubble sort on the processes vector. 
+// Helper function #1
+// Performs a bubble sort on the processes vector for the ready queue
 // It sorts primarily by arrival time (Ascending), and uses the process id
-// (Ascending) as a tie-breaker if multiple processes arrive at the exact same time.
+// Ascending process id as a tie-breaker if multiple processes arrive at the exact same time.
 void sortProcesses(vector<Process> &procs) {
     int n = procs.size();
     
@@ -33,15 +34,19 @@ void sortProcesses(vector<Process> &procs) {
         for (int j = 0; j < n - i - 1; j++) {
             bool shouldSwap = false;
             
+            // Sort by earliest arrival time 
             if (procs[j].arrivalTime > procs[j+1].arrivalTime) {
                 shouldSwap = true;
             } 
+
+            // If arrrival times are the same, lower process id goes first
             else if (procs[j].arrivalTime == procs[j+1].arrivalTime) {
                 if (procs[j].id > procs[j+1].id) {
                     shouldSwap = true;
                 }
             }
-
+            
+            // Swapping
             if (shouldSwap) {
                 Process temp = procs[j];
                 procs[j] = procs[j+1];
@@ -51,6 +56,8 @@ void sortProcesses(vector<Process> &procs) {
     }
 }
 
+// Helper function #2
+// For printing the output to avoid redundancy
 void printOutput(vector<Process> procs, int currentTime) {
     int n = procs.size();
 
@@ -60,6 +67,7 @@ void printOutput(vector<Process> procs, int currentTime) {
     double totalWait = 0, totalTurnaround = 0, totalResponse = 0;
     int totalBurst = 0;
     
+    // Re-sort the process by ID so the final output list is in order
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (procs[j].id > procs[j+1].id) {
@@ -79,8 +87,9 @@ void printOutput(vector<Process> procs, int currentTime) {
     
     cout << "Total CPU burst time: " << totalBurst << "ns" << endl;
 
-    double utilization = (currentTime > 0) ? ((double)totalBurst / currentTime) * 100.0 : 0.0;
-    cout << "CPU Utilization: " << utilization << "%" << endl;
+    // Ternary operator to prevent division by 0
+    double utilization = (currentTime > 0) ? ((double)totalBurst / currentTime) * 100.0 : 0.0; 
+    cout << "CPU Utilization: " << utilization << "%" << endl; 
 
     double throughput = (currentTime > 0) ? ((double)n / currentTime) : 0.0;
     cout << "Throughput: " << throughput << " processes/ns" << endl;
@@ -99,12 +108,14 @@ void printOutput(vector<Process> procs, int currentTime) {
 
 }
 
+// First Come First Serve
 void fcfs(vector<Process> procs, int testCaseNum) {
     sortProcesses(procs);
     cout << testCaseNum << " FCFS" << endl;
     int currentTime = 0;
     
     for (int i = 0; i < procs.size(); i++) {
+        // If the CPU is idle, jump time forward
         if (currentTime < procs[i].arrivalTime) {
             currentTime = procs[i].arrivalTime;
         }
@@ -112,9 +123,11 @@ void fcfs(vector<Process> procs, int testCaseNum) {
         procs[i].startTime = currentTime;
         cout << currentTime << " " << procs[i].id << " " << procs[i].burstTime << "X" << endl;
         
+        // Fast forward time by the burst time amount
         currentTime = currentTime + procs[i].burstTime;
         procs[i].finishTime = currentTime;
         
+        // Calculate the outputs
         procs[i].turnaroundTime = procs[i].finishTime - procs[i].arrivalTime;
         procs[i].waitingTime = procs[i].startTime - procs[i].arrivalTime;
         procs[i].responseTime = procs[i].startTime - procs[i].arrivalTime;
@@ -123,6 +136,7 @@ void fcfs(vector<Process> procs, int testCaseNum) {
     printOutput(procs, currentTime);
 }
 
+// Shortest Job First
 void sjf(vector<Process> procs, int testCaseNum) {
     sortProcesses(procs);
     cout << testCaseNum << " SJF" << endl;
@@ -134,19 +148,25 @@ void sjf(vector<Process> procs, int testCaseNum) {
     while (completedCount < n) {
         int shortestIdx = -1;
         
+        // Scan the ready queue for process that have arrived and aren't done
         for (int i = 0; i < n; i++) {
             if (procs[i].arrivalTime <= currentTime && !procs[i].isCompleted) {
+
+                // If this is the first valid process, set it as shortest
                 if (shortestIdx == -1) {
                     shortestIdx = i;
                 } 
                 else {
+                    // Find process with the smallest total burst time
                     if (procs[i].burstTime < procs[shortestIdx].burstTime) {
                         shortestIdx = i;
                     }
+                    // Tie breaker 1: If burst time matches, pick the earliest arrival
                     else if (procs[i].burstTime == procs[shortestIdx].burstTime) {
                         if (procs[i].arrivalTime < procs[shortestIdx].arrivalTime) {
                             shortestIdx = i;
-                        } 
+                        }
+                        // Tie breaker 2: If arrival match, pick the lower ID
                         else if (procs[i].arrivalTime == procs[shortestIdx].arrivalTime) {
                             if (procs[i].id < procs[shortestIdx].id) {
                                 shortestIdx = i;
@@ -157,19 +177,23 @@ void sjf(vector<Process> procs, int testCaseNum) {
             }
         }
 
+        // If a process was found in the ready queue
         if (shortestIdx != -1) {
             procs[shortestIdx].startTime = currentTime;
             cout << currentTime << " " << procs[shortestIdx].id << " " << procs[shortestIdx].burstTime << "X" << endl;
 
+            // Burst
             currentTime = currentTime + procs[shortestIdx].burstTime;
             procs[shortestIdx].finishTime = currentTime;
             procs[shortestIdx].isCompleted = true;
             completedCount++;
             
+            // Calculate output
             procs[shortestIdx].turnaroundTime = procs[shortestIdx].finishTime - procs[shortestIdx].arrivalTime;
             procs[shortestIdx].waitingTime = procs[shortestIdx].startTime - procs[shortestIdx].arrivalTime;
             procs[shortestIdx].responseTime = procs[shortestIdx].startTime - procs[shortestIdx].arrivalTime;
         } else {
+            // If the ready queue is empty, jump time to the next process arrival
             int nextArrival = -1;
             for (int i = 0; i < n; i++) {
                 if (!procs[i].isCompleted && procs[i].arrivalTime > currentTime) {
@@ -190,6 +214,7 @@ void sjf(vector<Process> procs, int testCaseNum) {
    printOutput(procs, currentTime);
 }
 
+// Shortest Remaining Time First
 void srtf(vector<Process> procs, int testCaseNum) {
     sortProcesses(procs);
     cout << testCaseNum << " SRTF" << endl;
@@ -202,18 +227,22 @@ void srtf(vector<Process> procs, int testCaseNum) {
     int startBlockTime = 0;
     int currentBurst = 0;
 
+    // Simulate time nanosecond by nanosecond to check for preemptions
     while (completedCount < n) {
         int shortestIdx = -1;
         
+        // Scan the ready queue for the process with the shortest remaining time 
         for (int i = 0; i < n; i++) {
             if (procs[i].arrivalTime <= currentTime && !procs[i].isCompleted) {
                 if (shortestIdx == -1) {
                     shortestIdx = i;
                 } 
                 else {
+                    // Condition 1: A new process has less remaining time than the current one
                     if (procs[i].remainingTime < procs[shortestIdx].remainingTime) {
                         shortestIdx = i;
                     }
+                    // Tie breakers with arrival time and process ID
                     else if (procs[i].remainingTime == procs[shortestIdx].remainingTime) {
                         if (procs[i].arrivalTime < procs[shortestIdx].arrivalTime) {
                             shortestIdx = i;
@@ -229,6 +258,7 @@ void srtf(vector<Process> procs, int testCaseNum) {
         }
 
         if (shortestIdx != -1) {
+            // Context switch
             if (lastProcessId != -1 && procs[shortestIdx].id != lastProcessId) {
                 cout << startBlockTime << " " << lastProcessId << " " << currentBurst << endl;
                 startBlockTime = currentTime;
@@ -237,14 +267,17 @@ void srtf(vector<Process> procs, int testCaseNum) {
 
             lastProcessId = procs[shortestIdx].id;
 
+            // Only log the start time the very first time the process gets the CPU
             if (procs[shortestIdx].startTime == -1) {
                 procs[shortestIdx].startTime = currentTime;
             }
 
+            // Run for 1 nanosecond
             procs[shortestIdx].remainingTime--;
             currentBurst++;
             currentTime++;
 
+            // Check if process finished its total burst
             if (procs[shortestIdx].remainingTime == 0) {
                 procs[shortestIdx].finishTime = currentTime;
                 procs[shortestIdx].isCompleted = true;
@@ -252,21 +285,24 @@ void srtf(vector<Process> procs, int testCaseNum) {
                 
                 cout << startBlockTime << " " << procs[shortestIdx].id << " " << currentBurst << "X" << endl;
                 
+                // Calculate output
                 procs[shortestIdx].turnaroundTime = procs[shortestIdx].finishTime - procs[shortestIdx].arrivalTime;
                 procs[shortestIdx].waitingTime = procs[shortestIdx].turnaroundTime - procs[shortestIdx].burstTime;
                 procs[shortestIdx].responseTime = procs[shortestIdx].startTime - procs[shortestIdx].arrivalTime;
 
+                // Reset variables
                 lastProcessId = -1; 
                 startBlockTime = currentTime;
                 currentBurst = 0;
                 }
             } else {
+            // If CPU is idle
             if (lastProcessId != -1) {
                  cout << startBlockTime << " " << lastProcessId << " " << currentBurst << endl;
                  lastProcessId = -1;
                  currentBurst = 0;
             }
-            
+            // Fast forward time to the next arrival
             int nextArrival = -1;
             for (int i = 0; i < n; i++) {
                 if (!procs[i].isCompleted && procs[i].arrivalTime > currentTime) {
@@ -282,6 +318,7 @@ void srtf(vector<Process> procs, int testCaseNum) {
                 currentTime++;
             }
             
+            // Sync block start time to account for the gap due to idle
             if (currentBurst == 0) {
                 startBlockTime = currentTime;
             }
